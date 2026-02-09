c06b211355cdab0c927b840ac3c6f4e45ee6439c
michaelaaronluyun <michael_aaron_luyun@dlsu.edu.ph>
2026-02-09 17:52:24 +0800

    so it can run

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,8 +42,12 @@ android {
     compileOptions {
         isCoreLibraryDesugaringEnabled = true
 
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+
+    kotlinOptions {
+        jvmTarget = "21"
     }
 
     buildFeatures {
